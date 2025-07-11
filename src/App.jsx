f4c4ebdd3edcb5e0d8f13a0c4fc4128e5918e2cb
anabel <anabelcurry@gmail.com>
2025-07-10 21:08:36 -0700

    the beginnings

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+ import './App.css'
+import tvAnimation from './assets/under-constructionTV.gif'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <div >
+        <img src={tvAnimation} className="tv" alt="tv-animation" />
+      </div>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>more coming soon!</h1>
+        <h2>new site under construction</h2>
       </div>
-      <h1>Vite + React</h1>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button 
+          href="src/assets/anabel-curry-resume-2025.pdf"
+          download="anabel-curry-resume-2025.pdf" 
+        >
+          download resume</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
